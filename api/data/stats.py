c47a0b6249ab8376f97bd78134b33a473618312c
LedoKun <romamp100@gmail.com>
2019-01-22 06:55:55 +0700

    Remove Ixstats

--- api/data/stats.py
+++ api/data/stats.py
@@ -4,7 +4,7 @@ from flask_jwt_extended import jwt_required
 
 from api.utils.stats.patients import PatientStats
 from api.utils.stats.visits import VisitStats
-from api.utils.stats.investigation import IxStats
+# from api.utils.stats.investigation import IxStats
 
 
 class Stats(Resource):
@@ -12,7 +12,7 @@ class Stats(Resource):
     def get(Resource):
         patients = PatientStats()
         visits = VisitStats()
-        ix = IxStats()
+        # ix = IxStats()
 
         return jsonify(
             {
@@ -49,7 +49,7 @@ class Stats(Resource):
                 "df_vaccination": visits.getCountEach(
                     dfField=["vaccination"], name="Vaccine"
                 ),
-                "df_init_cd4": ix.getCD4(isInit=True),
-                "df_cd4": ix.getCD4(isInit=False),
+                "df_init_cd4": None,
+                "df_cd4": None,
             }
         )
